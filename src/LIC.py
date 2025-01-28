5b3bb058914e4160dfa1335128dff1d241fad32b
Anton Yderberg <yderberg@kth.se>
2025-01-28 21:13:33 +0100

    feat: Implemented LIC9 #13
    Implement LIC 9 and added testing. Using Tangs function for calculating angels. Returns false if any value is not withnin parameters or

--- src/LIC.py
+++ src/LIC.py
@@ -22,3 +22,22 @@ def LIC2(POINTS, EPSILON):
             return True
     
     return False
+
+# Write a function to check if there exists a set of
+# three points separated by C_PTS and D_PTS that form 
+# an angle smaller than PI - EPSILON or greater than PI + EPSILON
+def LIC9(POINTS, EPSILON, C_PTS, D_PTS):
+    
+    if len(POINTS) < 5 or C_PTS < 1 or D_PTS < 1:
+        return False
+    if EPSILON < 0:
+        return False
+    
+    PI = 3.1415926
+    for i in range(len(POINTS) - C_PTS - D_PTS - 2):
+        angle = calc_angle(POINTS[i], POINTS[i+C_PTS+1], POINTS[i+C_PTS+D_PTS+2])
+        if angle == None:
+            continue
+        if angle < (PI - EPSILON) or angle > (PI + EPSILON):
+            return True
+    return False
