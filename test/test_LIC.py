f5f16f4df80e48095291477b9481f58238c280f9
Tang Shangxuan <shatan@kth.se>
2025-01-29 11:26:18 +0100

    feat: Implement LIC 8 #11
    This implements LIC 8 with tests.

--- test/test_LIC.py
+++ test/test_LIC.py
@@ -6,8 +6,9 @@ from src.LIC import (
     LIC3,
     LIC4,
     LIC5,
-    LIC9,
     LIC6,
+    LIC8,
+    LIC9,
     LIC12
 
 )
@@ -24,7 +25,6 @@ from src.LIC import (
         
     ]
 )
-
 def test_LIC0(POINTS, LENGTH1, expected_result):
     result = LIC0(LENGTH1, POINTS )
     assert result == expected_result
@@ -48,7 +48,6 @@ def test_LIC0(POINTS, LENGTH1, expected_result):
         ([(-1, -1), (-2, -2), (-3, -3)], 2.0, False),
     ]
 )
-
 def test_LIC1(POINTS, RADIUS1, expected_result):
     result = LIC1(POINTS, RADIUS1)
     assert result == expected_result
@@ -69,12 +68,11 @@ def test_LIC1(POINTS, RADIUS1, expected_result):
         ([(0, 0), (1, 1), (2, 2), (3, 0)], -0.1, False),
     ]
 )
-
 def test_LIC2(POINTS, EPSILON, expected_result):
     result = LIC2(POINTS, EPSILON)
     assert result == expected_result
     
-    #create testcases for LIC3
+#create testcases for LIC3
 @pytest.mark.parametrize(
     "AREA1, POINTS, expected_result",
     [
@@ -92,38 +90,32 @@ def test_LIC2(POINTS, EPSILON, expected_result):
         (-1.0, [(0, 0), (4, 0), (0, 3)], False),
     ]
 )
-
 def test_LIC3(AREA1, POINTS, expected_result):
     result = LIC3(AREA1, POINTS)
     assert result == expected_result
 
 @pytest.mark.parametrize(
-    "POINTS, EPSILON, C_PTS, D_PTS, expected_result",
+    "points, Q_PTS, QUADS, expected_result",
     [
-        # Positive case.
-        ([(0, 0), (1, 1), (2, 2), (3, 0), (4, 0)], 0.1, 1, 1, True), 
+        # Negatice case: Small Q_PTS, all points in one quadrant
+        ([(1,1), (2,2), (3,3)], 2, 1, False),
 
-        # Positive case 1, no angle is smaller than (PI - EPSILON).
-        ([(0, 0), (1.2, 1.2), (2, 2), (4.5, 4.5), (5, 0)], 0.1, 1, 1, False), 
+        # Positice case: Two consecutive points in different quadrants
+        ([(1,1), (-1,1)], 2, 1, True),
 
-        # Negative case 2, point[0] coincides with point[1]
-        ([(0, 0), (0, 0), (1, 1), (3, 0), (4, 0)], 0.1, 1, 1, False),
+        # Positive case: Three consecutive points crossing 3 quadrants 
+        ([(1,1), (-1,2), (0,-1), (1,-2)], 3, 2, True),
 
-        # Negative case 3, EPSILON < 0
-        ([(0, 0), (1, 1), (2, 2), (3, 0), (4, 0)], -0.1, 1, 1, False),
+        # Negatice case: Invalid Q_PTS < 2
+        ([(1,1), (2,2)], 1, 1, False),
 
-        # Negative case 4, C_PTS < 1
-        ([(0, 0), (1, 1), (2, 2), (3, 0), (4, 0)], 0.1, 0, 1, False),
-
-        # Negative case 5, D_PTS < 1
-        ([(0, 0), (1, 1), (2, 2), (3, 0), (4, 0)], 0.1, 1, 0, False),
+        # Negatice case: Q_PTS > number of points
+        ([(1,1), (2,2)], 3, 1, False),
     ]
 )
-    
-def test_LIC9(POINTS, EPSILON, C_PTS, D_PTS, expected_result):
-    result = LIC9(POINTS, EPSILON, C_PTS, D_PTS)
+def test_LIC4(points, Q_PTS, QUADS, expected_result):
+    result = LIC4(points, Q_PTS, QUADS)
     assert result == expected_result
-
 
 @pytest.mark.parametrize(
     "POINTS, expected_result",
@@ -135,7 +127,6 @@ def test_LIC9(POINTS, EPSILON, C_PTS, D_PTS, expected_result):
         ([(1.3, 2.5), (3.4, 5.5), (10, 0), (100.1, 3.3)], False)
     ]
 )
-
 def test_LIC5(POINTS, expected_result):
     result = LIC5(POINTS)
     assert result == expected_result
@@ -163,36 +154,56 @@ def test_LIC5(POINTS, expected_result):
         (3, [(0, 0), (0, 3), (0, 0)], 10, False),
     ]
 )
-
 def test_LIC6(N_PTS, POINTS, DIST, expected_result):
     result = LIC6(N_PTS, POINTS, DIST)
     assert result == expected_result
 
-
 @pytest.mark.parametrize(
-    "points, Q_PTS, QUADS, expected_result",
+    "POINTS, A_PTS, B_PTS, RADIUS1, expected_result",
     [
-        # Negatice case: Small Q_PTS, all points in one quadrant
-        ([(1,1), (2,2), (3,3)], 2, 1, False),
+        # Positive case, There exists three points separated by exactly A_PTS and B_PTS points, 
+        # cannot be contained within or on a circle of RADIUS1
+        ([(0, 0), (1.2, 2.3), (4, 0), (0, 1), (0, 3)], 1, 1, 2.4, True),
 
-        # Positice case: Two consecutive points in different quadrants
-        ([(1,1), (-1,1)], 2, 1, True),
+        # Positive case, three points are collinear but still cannot be contained.
+        ([(0, 0), (1.2, 2.3), (4, 0), (0, 1), (5.5, 0)], 1, 1, 2.4, True),
 
-        # Positive case: Three consecutive points crossing 3 quadrants 
-        ([(1,1), (-1,2), (0,-1), (1,-2)], 3, 2, True),
+        # Negative case, There doesn't exist three points cannot be contained.
+        ([(0, 0), (1.2, 2.3), (4, 0), (0, 1), (0, 3)], 1, 1, 2.6, False),
 
-        # Negatice case: Invalid Q_PTS < 2
-        ([(1,1), (2,2)], 1, 1, False),
-
-        # Negatice case: Q_PTS > number of points
-        ([(1,1), (2,2)], 3, 1, False),
+        # Negative case, the parameter is not valid.
+        ([(0, 0), (1.2, 2.3), (4, 0), (0, 1), (0, 3)], 2, 1, 2.4, False),
     ]
 )
-
-def test_LIC4(points, Q_PTS, QUADS, expected_result):
-    result = LIC4(points, Q_PTS, QUADS)
+def test_LIC8(POINTS, A_PTS, B_PTS, RADIUS1, expected_result):
+    result = LIC8(POINTS, A_PTS, B_PTS, RADIUS1)
     assert result == expected_result
-    
+
+@pytest.mark.parametrize(
+    "POINTS, EPSILON, C_PTS, D_PTS, expected_result",
+    [
+        # Positive case.
+        ([(0, 0), (1, 1), (2, 2), (3, 0), (4, 0)], 0.1, 1, 1, True), 
+
+        # Positive case 1, no angle is smaller than (PI - EPSILON).
+        ([(0, 0), (1.2, 1.2), (2, 2), (4.5, 4.5), (5, 0)], 0.1, 1, 1, False), 
+
+        # Negative case 2, point[0] coincides with point[1]
+        ([(0, 0), (0, 0), (1, 1), (3, 0), (4, 0)], 0.1, 1, 1, False),
+
+        # Negative case 3, EPSILON < 0
+        ([(0, 0), (1, 1), (2, 2), (3, 0), (4, 0)], -0.1, 1, 1, False),
+
+        # Negative case 4, C_PTS < 1
+        ([(0, 0), (1, 1), (2, 2), (3, 0), (4, 0)], 0.1, 0, 1, False),
+
+        # Negative case 5, D_PTS < 1
+        ([(0, 0), (1, 1), (2, 2), (3, 0), (4, 0)], 0.1, 1, 0, False),
+    ]
+)
+def test_LIC9(POINTS, EPSILON, C_PTS, D_PTS, expected_result):
+    result = LIC9(POINTS, EPSILON, C_PTS, D_PTS)
+    assert result == expected_result
 
 @pytest.mark.parametrize(
     "length1, length2, k_pts, points, expected_result",
@@ -213,7 +224,6 @@ def test_LIC4(points, Q_PTS, QUADS, expected_result):
         (1, 80, -1, [(0, 0), (1, 1), (2, 2), (3, 0)], False),
     ]
 )
-
 def test_LIC12(length1, length2, k_pts, points, expected_result):
     result = LIC12(length1, length2, k_pts, points)
     assert result == expected_result
